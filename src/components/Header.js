dfae5e91520b7888c6637ae9a5a6ef1f86155fb1
Raj Shukla <rajshukla140@gmail.com>
2024-01-11 18:53:17 +0530

    More modification in header and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../logo.jpeg";
 const Title = ()=>{
     return (
     <a href="/">
-      <img className="logo" src="../logo.jpg" alt="logo"/>
+      <img className="logo" src="../logo.png" alt="logo"/>
     </a>
     )
   }
@@ -14,10 +14,10 @@ const Title = ()=>{
         <Title/>
         <div className="nav-items">
           <ul>
-            <li><Link className="nav-links" to='/'>Home</Link></li>
-            <li><Link className="nav-links" to='/about'>About</Link></li>
-            <li><Link className="nav-links" to='/contact'>Contact</Link></li>
-            <li><Link className="nav-links" to='cart'>Cart</Link></li>
+            <li><Link className="nav-links" to='/'>🏠 Home</Link></li>
+            <li><Link className="nav-links" to='/about'>📙 About</Link></li>
+            <li><Link className="nav-links" to='/contact'>📱 Contact</Link></li>
+            <li><Link className="nav-links" to='cart'>🛒 Cart</Link></li>
           </ul>
         </div>
   
