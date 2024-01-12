4145b12ff2e460c71b387d1d4a010d4b13a780b6
Raj <rajshukla140@gmail.com>
2024-01-12 23:58:18 +0530

    extra changes in css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,7 +4,7 @@ const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating })=>{
     return (
       <div className='card'>
         <img src={IMG_CON_URL+cloudinaryImageId} alt='card'/>
-        <h1>{name}</h1>
+        <h1>{name.substring(0,15)}</h1>
         <h3>{cuisines.join(', ')}</h3>
         <h4>{avgRating} stars</h4>
       </div>
